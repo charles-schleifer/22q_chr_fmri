80837d7b12438dfb09d85bcac62853b3c95f811b
charles-schleifer <charlie.schleifer@gmail.com>
2023-11-05 17:38:54 -0800

    update

---
+++ submit_ahba_permute_hoffman.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# name your job
+job="ahba_perm"
+
+# set your study directory
+logdir="/u/project/cbearden/data/scripts/charlie/22q_chr_fmri/git_exclude/logs"
+mkdir -p ${logdir}
+
+# choose how many jobs in array
+length=10000
+
+# path to script
+script="/u/project/cbearden/data/scripts/charlie/22q_chr_fmri/ahba_permute_hoffman.R"
+
+# qsub command
+qsub -cwd -V -N $job -o $logdir -e $logdir -t 1-${length}:1 -l h_data=16G,h_rt=4:00:00 $script
